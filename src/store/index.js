ae9ecdb54853523751384d994ee9ce4f1e71422c
Ilya <36714951+sparklingW@users.noreply.github.com>
2020-01-10 18:34:56 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,4 @@
 import { createStore } from 'redux';
 import mainReducer from "./reducers";
 
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-export const store = createStore(mainReducer, reduxDevTools);
+export const store = createStore(mainReducer);
